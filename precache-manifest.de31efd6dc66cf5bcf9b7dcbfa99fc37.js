f9cd1bd1390e76b4d791f03d29da05a002e29c87
Dire <dirediredev@gmail.com>
2019-08-30 11:32:07 +0300

    Updates

---
+++ precache-manifest.de31efd6dc66cf5bcf9b7dcbfa99fc37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1925e1c5b2cd9bf4d647a47062db3d39",
+    "url": "/blackstone/index.html"
+  },
+  {
+    "revision": "7ea9f38ac6b6209ee14d",
+    "url": "/blackstone/static/css/main.8f11a56b.chunk.css"
+  },
+  {
+    "revision": "c59b8b09234421e13ae3",
+    "url": "/blackstone/static/js/2.a595af0e.chunk.js"
+  },
+  {
+    "revision": "7ea9f38ac6b6209ee14d",
+    "url": "/blackstone/static/js/main.6df89071.chunk.js"
+  },
+  {
+    "revision": "f62cb7b43bebfd436b24",
+    "url": "/blackstone/static/js/runtime~main.bb60794d.js"
+  }
+]);
