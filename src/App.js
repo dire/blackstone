57540fa6b6a5e46936c6f0cdccc57f94c0945c8d
Dire <dirediredev@gmail.com>
2019-09-21 10:49:13 +0300

    Added Recharge behaviour simple description

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="header">
         <h1>Blackstone Fortress Companion</h1>
         <span className="slogan">Unofficial App in the 41st Millenium</span>
-        <span className="version-number tiny">v0.2.0</span>
+        <span className="version-number tiny">v0.2.1</span>
       </header>
       <div className="app-body">
         <CombatBoard />
