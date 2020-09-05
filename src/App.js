52e8d53bfa020551fcee95a0c320bdaee58e0309
Dire <dirediredev@gmail.com>
2020-09-05 10:46:31 +0300

    Fixed invalid version.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="header">
         <h1>Blackstone Fortress Companion</h1>
         <span className="slogan">Unofficial App in the 41st Millenium</span>
-        <span className="version-number tiny">v0.2.3.1</span>
+        <span className="version-number tiny">v0.2.3</span>
       </header>
       <div className="app-body">
         <CombatBoard />
